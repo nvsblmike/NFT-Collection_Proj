379a6212f0525f4c8d63f07069d358b5f2e82540
Nvsblmike <soundboylesh77@gmail.com>
2023-03-03 16:01:06 +0100

    made changes to hardhat-tutorial constants

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -581,4 +581,4 @@ export const abi =[
       "type": "receive"
     }
   ]
-export const NFT_CONTRACT_ADDRESS = "0x7D2AA70D06cf3fD26721fA27fB72850F056fA259"
+export const NFT_CONTRACT_ADDRESS = "0x477F10d8c44731049E1E611c171EeA59167233cd"
